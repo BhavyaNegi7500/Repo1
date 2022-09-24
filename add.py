0c61383c83886675407eea4ba56e90f55e070322
BhavyaNegi7500 <111840693+BhavyaNegi7500@users.noreply.github.com>
2022-09-24 13:48:10 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,7 @@
+a=int(input("enter the no."))
+b=int(input("enter 2nd no."))
+multiply=a*b
+add=a+b
+divide=a/b
+substract=a-b
+print("addition=",add, " Subsration =" ,  substract, "Division=" , divide, "Multiply=",multiply)
